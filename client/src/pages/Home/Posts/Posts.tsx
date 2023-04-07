49445c9615f8be9dbc43f4ad5389bdf7d4cd0df6
Hồ Lâm <holam9120@gmail.com>
2023-04-07 11:02:11 +0700

    small update

--- client/src/pages/Home/Posts/Posts.tsx
+++ client/src/pages/Home/Posts/Posts.tsx
@@ -1,4 +1,5 @@
-import { CircularProgress, Grid } from "@mui/material";
+import { CircularProgress, Grid, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import Post from "./Post/Post";
 
 interface PostsProps {
@@ -6,9 +7,37 @@ interface PostsProps {
   isLoading: boolean;
 }
 
+const splitArray = (arr: any[], len: number, parts: number) => {
+  const chunkSize = Math.ceil(len / parts);
+  const chunks = [];
+  for (let i = 0; i < len; i += chunkSize) {
+    chunks.push(arr.slice(i, i + chunkSize));
+  }
+  return chunks;
+};
+
 function Posts({ posts, isLoading }: PostsProps) {
+  const theme = useTheme();
   if (isLoading) return <CircularProgress />;
   const n = posts?.length ?? 1;
+
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMediumScreen = useMediaQuery(theme.breakpoints.between("md", "lg"));
+  const isLargeScreen = useMediaQuery(theme.breakpoints.between("lg", "xl"));
+  const isExtraLargeScreen = useMediaQuery(theme.breakpoints.up("xl"));
+
+  let parts = 1;
+  if (isSmallScreen) {
+    parts = 1;
+  } else if (isMediumScreen) {
+    parts = 2;
+  } else if (isLargeScreen) {
+    parts = 3;
+  } else if (isExtraLargeScreen) {
+    parts = 4;
+  }
+
+  const splitPosts = splitArray(posts, n, parts);
 
   return (
     <Grid
@@ -23,21 +52,22 @@ function Posts({ posts, isLoading }: PostsProps) {
         mb: 3.75,
       }}
     >
-      <Grid item sm={6} md={4} lg={3} display="flex" flexDirection="column">
-        {posts?.slice(0, n / 3)?.map((post) => (
-          <Post key={post._id} post={post} />
-        ))}
-      </Grid>
-      <Grid item sm={6} md={4} lg={3} display="flex" flexDirection="column">
-        {posts?.slice(n / 3, (2 * n) / 3)?.map((post) => (
-          <Post key={post._id} post={post} />
-        ))}
-      </Grid>
-      <Grid item sm={6} md={4} lg={3} display="flex" flexDirection="column">
-        {posts?.slice((2 * n) / 3)?.map((post) => (
-          <Post key={post._id} post={post} />
-        ))}
-      </Grid>
+      {splitPosts.map((postsPart, i) => (
+        <Grid
+          gap={3}
+          item
+          key={i}
+          sm={6}
+          md={4}
+          lg={3}
+          display="flex"
+          flexDirection="column"
+        >
+          {postsPart.map((post) => (
+            <Post key={post._id} post={post} />
+          ))}
+        </Grid>
+      ))}
     </Grid>
   );
 }
