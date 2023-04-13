d2066f905ad84461ea1854dff6a7fce343ae2a31
Hồ Lâm <holam9120@gmail.com>
2023-04-13 22:12:26 +0700

    msg

--- client/src/apis/authSlice.ts
+++ client/src/apis/authSlice.ts
@@ -11,7 +11,7 @@ const authSlice = baseApi.injectEndpoints({
         url: `${BASE_PATH}/login-post`,
         body: payload,
       }),
-      invalidatesTags: [queryTagTypes.POST],
+      invalidatesTags: [queryTagTypes.ALL_POST],
     }),
 
     signUp: builder.mutation<void, any>({
